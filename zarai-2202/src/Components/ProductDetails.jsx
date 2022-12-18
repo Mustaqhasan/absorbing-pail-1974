5d8eb48401f2698414e23c68a457de3508317847
Mustaqhasan <shaikhmustaq7714@gmail.com>
2022-12-19 00:44:05 +0530

    Update

--- zarai-2202/src/Components/ProductDetails.jsx
+++ zarai-2202/src/Components/ProductDetails.jsx
@@ -495,7 +495,7 @@ export default function ProductDetails() {
           })}
         </div>
         <div>
-          <img width={"300px"} src={mainPage.url} alt="" />
+          <img width={"300px"} src={img} alt="" />
         </div>
     </div>
   
