97bb4870be4a4b151d96aac231c8f7243d1980f4
Mustaqhasan <shaikhmustaq7714@gmail.com>
2022-12-18 22:25:18 +0530

    Up to date

--- zarai-2202/src/App.js
+++ zarai-2202/src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Navbar from './Components/Navbar';
-import Home from './Pages/Home';
 import AllRoutes from './Components/AllRoutes';
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar/> */}
-      {/* <Home/> */}
       <AllRoutes/>
     </div>
   );
